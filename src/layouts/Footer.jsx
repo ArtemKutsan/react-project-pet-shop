abed50d530de1bfc895d2d24c56b2288bf1bb9f6
Artem Kutsan <artemkutsan@gmail.com>
2026-04-17 15:27:19 +0200

    feat: добавлен компонент Footer

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,15 @@
+// src/layouts/Footer/index.jsx
+import Contact from '../features/Contact';
+import Map from '../features/Map';
+
+export default function Footer() {
+  return (
+    <footer className="bg-white py-14 md:py-20">
+      <div className="container max-w-380 space-y-8">
+        <h2 className="text-4xl font-bold tracking-tight md:text-6xl mb-10">Contact</h2>
+        <Contact />
+        <Map />
+      </div>
+    </footer>
+  );
+}
