ac5e671f55f2a4f7acc2755b9e4901837c17703c
Artem Kutsan <artemkutsan@gmail.com>
2026-04-17 11:44:19 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <main className="py-8">
+        <section>
+          <div className="prose container max-w-4xl">
+            <h1>React Project Pet Shop</h1>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
