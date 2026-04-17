3196038718a00f0a4bde36dd2a42c019eb17d540
Artem Kutsan <artemkutsan@gmail.com>
2026-04-17 14:20:19 +0200

    feat: создал компонент Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+
+export default function Hero() {
+  return (
+    <section className="relative min-h-150 flex bg-[url(/images/hero.jpg)] bg-cover bg-center">
+      <div className="relative container max-w-380">
+        <h1 className="mt-20 max-w-5xl leading-[1.1] text-4xl font-bold text-white md:text-8xl">
+          Amazing Discounts on Pets Products!
+        </h1>
+        <Link
+          to="/all-sales"
+          className="mt-8 inline-flex items-center rounded-sm bg-blue-600 px-14 py-4 text-xl font-semibold text-white transition-colors hover:bg-blue-700"
+        >
+          Check out
+        </Link>
+      </div>
+    </section>
+  );
+}
