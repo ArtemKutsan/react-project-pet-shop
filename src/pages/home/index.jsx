1eba70500cfb5d98d196214e3cec3c2d50367e82
Artem Kutsan <artemkutsan@gmail.com>
2026-04-17 16:25:19 +0200

    add: подключен компонент Discount

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,11 @@
 import Hero from '../../components/Hero';
+import Discount from '../../features/Discount';
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <Discount />
     </>
   );
 }
