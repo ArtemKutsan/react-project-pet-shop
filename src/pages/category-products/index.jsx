44e59c46430ac38a80a654e2b8b1873ac5ab9cc5
Artem Kutsan <artemkutsan@gmail.com>
2026-04-18 21:52:12 +0200

    feat: добавление компонента Products на страницы all-products и category-products

--- src/pages/category-products/index.jsx
+++ src/pages/category-products/index.jsx
@@ -1,7 +1,28 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import Products from '../../features/Products';
+import { loadProductsByCategory } from '../../store/productsSlice';
+
 export default function CategoryProductsPage() {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const { data, status, error } = useSelector((state) => state.products.byCategory);
+  const title = data.category?.title || 'Category products';
+  const products = data.data || [];
+
+  useEffect(() => {
+    if (id) {
+      dispatch(loadProductsByCategory(id));
+    }
+  }, [dispatch, id]);
+
   return (
-    <div className="container max-w-380 space-y-8 py-10">
-      <p>Товары категории</p>
-    </div>
+    <section className="py-14 md:py-20">
+      <div className="container max-w-380 grid gap-10">
+        <h2>{title}</h2>
+        <Products products={products} status={status} error={error} />
+      </div>
+    </section>
   );
 }
