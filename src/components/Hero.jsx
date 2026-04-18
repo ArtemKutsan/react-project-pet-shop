5a902f145b2bf9b645e9f5c1dd5f0c8ce930f485
Artem Kutsan <artemkutsan@gmail.com>
2026-04-18 18:47:47 +0200

    chore: незначительные изменения

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
         <h1 className="mt-20 max-w-5xl text-white">Amazing Discounts on Pets Products!</h1>
         <Link
           to="/all-sales"
-          className="mt-8 inline-flex items-center rounded-sm bg-blue-600 px-14 py-4 text-xl font-semibold text-white transition-colors hover:bg-blue-700"
+          className="mt-8 px-14 button text-xl font-semibold button-primary min-h-14"
         >
           Check out
         </Link>
