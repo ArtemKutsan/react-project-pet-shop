8439684fd8724c87b47a8b912a4e79eb44737c1b
Artem Kutsan <artemkutsan@gmail.com>
2026-04-18 11:33:43 +0200

    refactor: изменение верстки для секции Categories

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Hero from '../../components/Hero';
 import Categories from '../../features/Categories';
 import Discount from '../../features/Discount';
@@ -6,7 +7,22 @@ export default function HomePage() {
   return (
     <>
       <Hero />
-      <Categories />
+      <section className="bg-white py-14 md:py-20">
+        <div className="container max-w-380 grid gap-10">
+          <div className="flex flex-col md:flex-row md:items-center">
+            <h2 className="mb-4 md:mr-8 md:mb-0">Categories</h2>
+            <div className="hidden h-px grow bg-gray-200 md:flex" />
+          <Link
+            to="/categories"
+            className="flex min-h-10 items-center justify-center rounded-sm border border-gray-200 px-6 font-medium text-gray-400 transition-colors hover:border-gray-400 hover:text-gray-800"
+            >
+              All categories
+            </Link>
+          </div>
+
+          <Categories limit={4} />
+        </div>
+      </section>
       <Discount />
     </>
   );
