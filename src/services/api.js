9e2b4b7fac1a9aa7edf5f0aba9dc7e193b93af0a
Artem Kutsan <artemkutsan@gmail.com>
2026-04-19 10:32:17 +0200

    fix: исправление хелперов для загрузки данных, убран ошибочный хелпер для загрузки товаров со скидкой (такого нет на бекенде)

--- src/services/api.js
+++ src/services/api.js
@@ -10,5 +10,4 @@ export const fetchAllCategories = () => api.get('/categories/all');
 export const fetchProductsByCategory = (categoryId) => api.get(`/categories/${categoryId}`);
 export const fetchAllProducts = () => api.get('/products/all');
 export const fetchProductById = (id) => api.get(`/products/${id}`);
-export const fetchSaleProducts = () => api.get('/sale');
 export const submitOrder = (orderData) => api.post('/order/send', orderData);
