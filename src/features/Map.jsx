0f72946680dcf4ee684e52c2ae29935de8cf6fd3
Artem Kutsan <artemkutsan@gmail.com>
2026-04-24 06:47:04 +0200

    chore: уточнение координат

--- src/features/Map.jsx
+++ src/features/Map.jsx
@@ -4,8 +4,8 @@ export default function Map() {
   return (
     <div className="overflow-hidden rounded-xl">
       <iframe
-        title="Pet shop location map"
-        src="https://www.openstreetmap.org/export/embed.html?bbox=13.399%2C52.509%2C13.417%2C52.518&layer=mapnik&marker=52.5135%2C13.408"
+        title="Exact location map"
+        src="https://www.openstreetmap.org/export/embed.html?bbox=13.4020%2C52.5102%2C13.4068%2C52.5122&layer=mapnik&marker=52.511205931591384%2C13.404467871163769"
         className="aspect-square md:aspect-1360/350 w-full"
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
